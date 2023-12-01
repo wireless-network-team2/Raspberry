0bb24174774f1724b6d7830b88a86c9a5aff1803
taehyoun1219 <149992750+taehyoun1219@users.noreply.github.com>
2023-12-01 20:56:32 +0900

    Update water.py

--- water.py
+++ water.py
@@ -1,28 +1,32 @@
 import serial
-import time
-import firebase import firebase
+import firebase_admin
+from firebase_admin import credentials, db
 
 # Firebase 초기화
-firebase = firebase.FirebaseApplication('https://smartbowl-b05d9-default-rtdb.firebaseio.com/', None)
+cred = credentials.Certificate("/home/pi/project2/firebase/smartbowl-b05d9-firebase-adminsdk-ldgsu-b9626cd4c3.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://smartbowl-b05d9-default-rtdb.firebaseio.com/'})
+ref = db.reference('/data')
 
 # 시리얼 포트 설정
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while True:
     # 아두이노에서 값 읽어오기
-    arduino_data = ser.readline().decode('utf-8').rstrip().split(',')
+    arduino_data = ser.readline().decode('utf-8').strip()
+    
+    values = arduino_data.split(',')
+    print(values)
 
     # pH 값과 수위 값 추출
-    ph_value = float(arduino_data[0])
-    water_level = float(arduino_data[1])
+    ph_value = values[0]
+    water_level = values[1]
 
     # Firebase에 데이터 업로드
     data = {
         'waterdetection': ph_value,
         'watermeasurement': water_level
     }
+    
+    ref.set(data)
+    
 
-    result = firebase.post('data', data)
-    print(f"파이어베이스 업로드 완료 {result}")
-
-    time.sleep(10)  # 10초마다 데이터 업로드
