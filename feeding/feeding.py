dbec64e069a85c5e9dd59afd14adb1606bdcb82e
G2nie <202144081@itc.ac.kr>
2023-12-05 13:19:24 +0900

    Update feeding.py

--- feeding/feeding.py
+++ feeding/feeding.py
@@ -13,27 +13,27 @@ arduino_serial_port = '/dev/ttyACM0'
 ser = serial.Serial(arduino_serial_port, 9600)
 
 while True:
-    try:
-        # Firebase에서 데이터 읽기
-        data = ref.get()
+	try:
+		# Firebase에서 데이터 읽기
+		data = ref.get()
 
-        if data:
-            for key, value in data.items():
-                if value == 1:
-                    if key == "little":
-                        ser.write("L".encode('utf-8'))
-                        print("Little")
-                    elif key == "half":
-                        ser.write("H".encode('utf-8'))
-                        print("Half")
-                    elif key == "full":
-                        ser.write("F".encode('utf-8'))
-                        print("Full")
+		if data:
+			for key, value in data.items():
+				if value == 1:
+					if key == "little":
+						ser.write("L".encode('utf-8'))
+						print("Little")
+					elif key == "half":
+						ser.write("H".encode('utf-8'))
+						print("Half")
+					elif key == "full":
+						ser.write("F".encode('utf-8'))
+						print("Full")
 
-                    data[key] = 0
-                    requests.patch('https://smartbowl-b05d9.firebaseio.com/feeding.json', json={key: 0})
+					data[key] = 0
+					requests.patch('https://smartbowl-b05d9.firebaseio.com/feeding.json', json={key: 0})
 
-    except Exception as e:
-        print(f"Error: {e}")
+	except Exception as e:
+		print(f"Error: {e}")
 
-    time.sleep(1)
+	time.sleep(1)
