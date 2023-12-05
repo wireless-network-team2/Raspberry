5b7408e12652e3c46d047f227df7ccf4d7784c56
G2nie <202144081@itc.ac.kr>
2023-12-05 13:12:04 +0900

    Update feeding.py

--- feeding/feeding.py
+++ feeding/feeding.py
@@ -4,37 +4,37 @@ import requests
 import time
 import serial
 
-//firebase_url = 'https://smartbowl-b05d9.firebaseio.com/feeding.json'
 
-firebase_admin.initialize_app(cred, {'databaseURL': 'https://smartbowl-b05d9.firebaseio.com/feeding'})
-cred = credentials.Certificate("path/to/your/firebase/credentials.json")
-ref = db.reference('/data')
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://smartbowl-b05d9.firebaseio.com/'})
+cred = credentials.Certificate("p/home/user/team2/smartbowl-b05d9-firebase-adminsdk-ldgsu-b9626cd4c3.json")
+ref = db.reference('/feeding')
 
 arduino_serial_port = '/dev/ttyACM0'
 
 ser = serial.Serial(arduino_serial_port, 9600)
 
 while True:
-    # Firebase에서 데이터 읽기
-    data = ref.get()
-    
-    if data:
-        for key, value in data.items():
-            if value == 1:
-                if key == "little":
-                    ser.write("L".encode('utf-8'))
-                    print("Little")
-                elif key == "half":
-                    ser.write("H".encode('utf-8'))
-                    print("Half")
-                elif key == "full":
-                    ser.write("F".encode('utf-8'))
-                    print("Full")
-                    
-                data[key] = 0
-                requests.patch('https://your-firebase-project-id.firebaseio.com/data.json', json={key: 0})
+    try:
+        # Firebase에서 데이터 읽기
+        data = ref.get()
 
-    except Exception as e:
-        print(f"Error: {e}")
+        if data:
+			for key,value in data.items(): 
+				if value == 1:
+					if key == "little":
+						ser.write("L".encode('utf-8'))
+						print("Little")
+						elif key == "half":
+							ser.write("H".encode('utf-8'))
+							print("Half")
+							elif key == "full":
+								ser.write("F".encode('utf-8'))
+								print("Full")
+								
+				data[key] = 0
+				requests.patch('hhttps://smartbowl-b05d9.firebaseio.com/feeding.json', json={key: 0})
+
+	except Exception as e:
+		print(f"Error: {e}")
         
 time.sleep(1)
